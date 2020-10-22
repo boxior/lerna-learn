72e5f573f30da37bb394c1c035bc7ba17c061b1a
boxior <1987boxior@gmail.com>
2020-10-22 18:24:29 +0300

    remove import react app from common package

--- packages/common/index.js
+++ packages/common/index.js
@@ -1,9 +1,6 @@
 const _ = require("lodash");
 const server = require("@boxior/server");
 const React = require("react");
-const reactApp = require("@boxior/react-app");
-
-console.log("reactApp", reactApp);
 
 console.log("server", server);
 const a = {a: "A"};
